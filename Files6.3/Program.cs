ef095423556b26366c8e4c11750b6e08747871c2
Milkingooo <mask.trushin.06@bk.ru>
2024-04-01 21:19:24 +0300

    Add 6.3

--- Files6.3/Program.cs
+++ Files6.3/Program.cs
@@ -13,23 +13,48 @@ namespace ConsoleApplication1
         {
             try
             {
-                FileStream file = new FileStream("input.txt", FileMode.Create, FileAccess.ReadWrite);
-                int str = 0, stolb = 0;
-                Console.WriteLine("Введите размер матрицы: \n");
-                str = Console.Read();
-                stolb = Console.Read();
-                file.WriteByte((byte)str);
-                file.WriteByte((byte)stolb);
-                file.Close();
-                Console.ReadLine();
+                string[] lines = File.ReadAllLines("input.txt");
 
-                FileStream file1 = new FileStream("input.txt", FileMode.Open, FileAccess.ReadWrite);
-                file1.Seek(0, SeekOrigin.Begin); // текущий указатель - на начало
-                int a;
-                for (int i = 0; i < 2 ; i++)
+                string[] razmer = lines[0].Split(' ');
+                int rows = int.Parse(razmer[0]);
+                int cols = int.Parse(razmer[1]);
+
+                int[,] matrix = new int[rows, cols];
+
+                // Заполнение матрицы
+                for (int i = 0; i < rows; i++)
                 {
-                    a = file1.ReadByte();
-                    Console.Write((int)a + " "); //контрольная печать считанного значения
+                    string[] row = lines[i + 1].Split(' ');
+                    for (int j = 0; j < cols; j++)
+                    {
+                        matrix[i, j] = int.Parse(row[j]);
+                    }
+                }
+                int count = 0; 
+
+                //Нахождние удвоенных чисел
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        if (matrix[i, j] % 2 != 0)
+                        {
+                            matrix[i, j] = matrix[i, j] * 2;
+                            count++;
+                        }
+                    }
+                }
+                using (StreamWriter writer = new StreamWriter("output.txt"))
+                {
+                    writer.WriteLine(count);
+                    for (int i = 0; i < rows; i++)
+                    {
+                        for (int j = 0; j < cols; j++)
+                        {
+                            writer.Write(matrix[i, j] + " ");
+                        }
+                        writer.WriteLine();
+                    }
                 }
 
             }
